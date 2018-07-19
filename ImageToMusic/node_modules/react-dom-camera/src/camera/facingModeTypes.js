3020246e2608ef532f028d2ebe26b65f466ac8b1
Abdul Rehman Liaqat <arl@plista.com>
2018-07-19 16:54:44 +0200

    remove useless files

--- ImageToMusic/node_modules/react-dom-camera/src/camera/facingModeTypes.js
+++
@@ -1,6 +0,0 @@
-export const facingModes = {
-  ENVIRONMENT: 'ENVIRONMENT',
-  LEFT: 'LEFT',
-  RIGHT: 'RIGHT',
-  USER: 'USER',
-};
--- ImageToMusic/node_modules/redux-form-antd/stories/Center.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default story => (
-  <div style={{ display: "flex", justifyContent: "center", marginTop: 100 }}>
-    {story()}
-  </div>
-);
