21a8c9dff821456727ec5f46153df1b4a2f1b219
Abdul Rehman Liaqat <arl@plista.com>
2018-07-21 23:29:17 +0200

    machine learning prediction

--- ISARC_2018_hackathon/app/app.py
+++ ISARC_2018_hackathon/app/app.py
@@ -6,8 +6,53 @@ Created on Sat Jul 21 10:30:28 2018
 @author: abdulliaqat
 """
 
-import requests
-import panadas as pd
+import pandas as pd
 import numpy as np
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from sodapy import Socrata
+import requests
 
 
+df = pd.read_csv('pollution_data1.csv')
+m = Basemap(projection='mill',
+            llcrnrlat = 40,
+            llcrnrlon = -130,
+            urcrnrlat = 50,
+            urcrnrlon = -60,
+            resolution = 'l')
+m.drawcoastlines()
+m.drawcountries(linewidth=2)
+m.drawstates(color='b')
+#m.fillcontinents()
+#m.bluemarble
+
+nyclat, nyclon = 40.7127, -74.0059
+xpt,ypt = m(nyclon,nyclat)
+#m.plot(xpt,ypt,'g^',markersize = 25)
+m.scatter(xpt, ypt,marker='D',color='m')
+plt.title('Basemap tutorial')
+plt.show()
+
+
+
+client = Socrata("data.cityofnewyork.us", None)
+restults = client.get("i4gi-tjb9", where="data_as_of > '2018-07-01T00:00:00.000' and borough in ('Brookly','Manhattan','Queens')",limit='2000000')
+results_df = pd.DataFrame.from_records(restults)
+results_df['minutes'] = results_df['data_as_of'].apply(lambda x:True if(int(x[14:16])%5 in (1,4,0)) else False)
+final_df = results_df[results_df['minutes'] == True]
+final_df['data_point'] = final_df['link_points'].apply(lambda x:x.split(' ')[1] if(x.split(' ')[1] != '') else x.split(' ')[2])
+points_of_consideration = final_df['data_point'].unique()
+points_of_consideration = [[float(i) for i in point.split(',')] for point in points_of_consideration]
+#df.datetime = pd.datetime(df.datetime)
+#df.index = df.datetime
+groups = df.groupby(['lat','long'])
+for group in groups.all().index:
+    one_group = groups.get_group(group)
+    plt.plot(one_group['pm10'])
+    plt.plot(one_group['pm25'])
+    plt.plot(one_group['co'])
+    plt.plot(one_group['o3'])
+    plt.plot(one_group['no2'])
+    plt.show()
+    print(one_group['pm10'].iloc[10])
--- ISARC_2018_hackathon/app/preprocessing.py
+++ ISARC_2018_hackathon/app/preprocessing.py
@@ -7,13 +7,14 @@ Created on Sat Jul 21 10:30:28 2018
 """
 
 import requests
-import panadas as pd
+import pandas as pd
 import numpy as np
+import pickle
 
 
 
 content=[]
-lat_long_list = [[40.850247,-73.895282],
+old_lat_long_list = [[40.850247,-73.895282],
 [40.959615	,-73.80861],
 [40.811097	,-73.878641],
 [40.802965	,-73.90999],
@@ -25,23 +26,94 @@ lat_long_list = [[40.850247,-73.895282],
 [40.703188	,-73.78754],
 [40.641238	,-73.778021]]
 
+
+
+
+lat_long_list = [[40.846951, -73.933641],
+ [40.745616, -73.97305],
+ [40.7415, -73.95491001],
+ [40.817516, -73.80261],
+ [40.80218, -73.79324001],
+ [40.7727804, -73.83087],
+ [40.8006, -73.82895],
+ [40.7252805, -74.01128],
+ [40.77367, -73.92198],
+ [40.7738304, -73.92197],
+ [40.730165, -73.87643],
+ [40.78647, -73.78812],
+ [40.7604, -74.00328],
+ [40.76017, -74.00382],
+ [40.7604506, -74.003221],
+ [40.7605, -74.0032],
+ [40.737015, -73.85373001],
+ [40.66738, -73.77021],
+ [40.66579, -73.75706],
+ [40.68314, -73.72692],
+ [40.8492, -73.945241],
+ [40.7084705, -73.99884],
+ [40.70895, -73.996941],
+ [40.70158, -73.99033],
+ [40.69236, -73.99939],
+ [40.76212, -74.91],
+ [40.74137, -74.00893],
+ [40.73651, -73.975011],
+ [40.7601606, -73.95751],
+ [40.73868, -73.973391],
+ [40.712, -73.97793],
+ [40.73895, -74.01012],
+ [40.79751, -73.827091],
+ [40.67374, -73.80154],
+ [40.70538, -74.01528],
+ [40.6822, -74.0057201],
+ [40.7894, -73.78765],
+ [40.79771, -73.92004],
+ [40.77391, -73.9222],
+ [40.80151, -73.93066],
+ [40.8013005, -73.930181],
+ [40.793425, -73.79344],
+ [40.7889404, -73.789571],
+ [40.75605, -73.740851],
+ [40.7454306, -73.76907],
+ [40.7887906, -73.78895],
+ [40.7868604, -73.78838],
+ [40.7719, -73.99401],
+ [40.66642, -73.78958],
+ [40.68361, -73.72646],
+ [40.7136604, -73.7292],
+ [40.71309, -73.72892],
+ [40.74799, -73.73784],
+ [40.763521, -73.99935],
+ [40.7713004, -73.99455],
+ [40.78918, -73.78792],
+ [40.77065, -73.834421],
+ [40.76191, -73.839121],
+ [40.7270705, -73.83231],
+ [40.700841, -73.815751],
+ [40.7268904, -73.83239],
+ [40.7613906, -73.83898],
+ [40.7702704, -73.8354],
+ [40.77384, -73.92192]]
+
 for location in lat_long_list:
     content.append([])
-    for i in range(1,20):
+    for i in range(10,15):
         start_date = '2018-07-{}T00:00:00'.format(i) 
         end_date = '2018-07-{}T00:00:00'.format(i+1) 
         req_variable = r"https://api.breezometer.com/baqi/?end_datetime={}&interval=1&key=de4fef0f7fb349f29f3f21c275018069&lang=en&lat={}&lon={}&start_datetime={}".format(end_date,location[0],location[1],start_date)
         content[-1].append(requests.get(req_variable).json())
-        print(len(content[-1][-1]))
-"https://api.breezometer.com/baqi/?end_datetime={}&interval=1&key=de4fef0f7fb349f29f3f21c275018069&lang=en&lat={}&lon={}&start_datetime={}".format(end_date,location[0],location[1],start_date)
-"https://api.breezometer.com/baqi/?lat=40.7324296&lon=-73.9977264&interval=1&start_datetime=2018-07-01T00:00:00&end_datetime=2018-07-02T00:00:00&key=e7531d2d67894bd7a2c6eaf7f950c374"
-"https://api.breezometer.com/baqi/?end_datetime=2018-07-02T00:00:00&interval=1&key=de4fef0f7fb349f29f3f21c275018069&lang=en&lat=40.7324296&lon=-73.9977264&start_datetime=2018-07-01T00:00:00"
-start_date = '2018-07-20T00:00:00'
-end_date = '2018-07-02T00:00:00'
+        with open('traffic_data_pollution_3.obj','wb') as f:
+            pickle.dump(content,f)
+        print(len(content[-1][-1]),i,location)
+
+#"https://api.breezometer.com/baqi/?end_datetime={}&interval=1&key=de4fef0f7fb349f29f3f21c275018069&lang=en&lat={}&lon={}&start_datetime={}".format(end_date,location[0],location[1],start_date)
+#"https://api.breezometer.com/baqi/?lat=40.7324296&lon=-73.9977264&interval=1&start_datetime=2018-07-01T00:00:00&end_datetime=2018-07-02T00:00:00&key=e7531d2d67894bd7a2c6eaf7f950c374"
+#"https://api.breezometer.com/baqi/?end_datetime=2018-07-02T00:00:00&interval=1&key=de4fef0f7fb349f29f3f21c275018069&lang=en&lat=40.7324296&lon=-73.9977264&start_datetime=2018-07-01T00:00:00"
+#start_date = '2018-07-20T00:00:00'
+#end_date = '2018-07-02T00:00:00'
 #req = r"https://api.breezometer.com/baqi/?lat=40.7324296&lon=-73.9977264&interval=1&start_datetime=2018-07-10T00:00:00&end_datetime=2018-07-11T05:25:00&key=e7531d2d67894bd7a2c6eaf7f950c374"
-req_variable = r"https://api.breezometer.com/baqi/?lat=40.7324296&lon=-73.9977264&interval=1&datetime={}&key=e7531d2d67894bd7a2c6eaf7f950c374".format(start_date)
-req_hack = r"https://api.breezometer.com/baqi/?end_datetime={}&interval=1&key=de4fef0f7fb349f29f3f21c275018069&lang=en&lat={}&lon={}&start_datetime={}".format(end_date,location[0],location[1],start_date)
-content = requests.get(req_hack)
+#req_variable = r"https://api.breezometer.com/baqi/?lat=40.7324296&lon=-73.9977264&interval=1&datetime={}&key=e7531d2d67894bd7a2c6eaf7f950c374".format(start_date)
+#req_hack = r"https://api.breezometer.com/baqi/?end_datetime={}&interval=1&key=de4fef0f7fb349f29f3f21c275018069&lang=en&lat={}&lon={}&start_datetime={}".format(end_date,location[0],location[1],start_date)
+#content = requests.get(req_hack)
 
 
 pollution_data_columns = ['datetime','lat','long','co','no2','o3','pm10','pm25','so2']
@@ -64,4 +136,4 @@ for location,con in zip(lat_long_list,content):
                 so2 = hour_data['pollutants']['so2']['concentration']
                 pollution_data.loc[len(pollution_data)] = [datetime,lat,long,co,no2,o3,pm10,pm25,so2]           
                 
-pollution_data.to_csv('pollution_data1.csv',index = False)
+pollution_data.to_csv('pollution_and_traffic_data10_14.csv',index = False)
